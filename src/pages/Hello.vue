1d7c22d1982989e153e74295dd777c1777d33212
何永能 <yongneng.he@dianping.com>
2017-06-06 20:04:23 +0800

    examples

--- src/pages/Hello.vue
+++ src/pages/Hello.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="hello">
     <p>欢迎 {{ msg }}!</p>
+    <p>也可以这样绑定 <span v-text='msg'/></p>
+    <p v-bind:title='message'>鼠标悬停几秒钟查看此处动态绑定的提示信息！</p>
   </section>
 </template>
 
@@ -9,7 +11,8 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Henry'
+      msg: 'Henry',
+      message: '页面加载于 ' + new Date()
     }
   }
 }
